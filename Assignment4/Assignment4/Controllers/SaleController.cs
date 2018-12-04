d23127e7b2d5883850f0c615b338107891d12994
Navdeep Kaur <navdeepkaur5077@gmail.com>
2018-12-03 22:43:40 -0500

    Added Form

---
+++ Assignment4/Assignment4/Controllers/SaleController.cs
@@ -0,0 +1,63 @@
+﻿using Assignment4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment4.Controllers
+{
+    public class SaleController : Controller
+    {
+        Assignment4Entities db;
+        // GET: Sale
+        public ActionResult Index()
+        {
+            
+            db = new Assignment4Entities();
+
+            var obj = (from s in db.saleinfoes
+                       select new salesinfo
+                       {
+                           _name = s.name,
+                           _contact_no = s.contact_no,
+                           _contact_email = s.contact_email,
+                           _address = s.address,
+                           _city = s.city,
+                           _car_make = s.car_make,
+                           _car_model = s.car_model,
+                           _car_year = s.car_year
+                       }).ToList<salesinfo>();
+
+            db.Dispose();
+            return View(obj);
+        }
+
+        public ActionResult AddInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddInfo(salesinfo obj)
+        {
+            saleinfo oSel = new saleinfo()
+            {
+                name = obj._name,
+                address = obj._address,
+                city = obj._city,
+                contact_email = obj._contact_email,
+                contact_no = obj._contact_no,
+                car_make = obj._car_make,
+                car_model = obj._car_model,
+                car_year = obj._car_year
+            };
+
+            db = new Assignment4Entities();
+            db.saleinfoes.Add(oSel);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Assignment4/Assignment4/Models/salesinfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Assignment4.Models
+{
+    public class salesinfo
+    {
+
+        public int _id { get; set; }
+        [DisplayName("Name")]
+        public string _name { get; set; }
+        [DisplayName("Contact No")]
+        public string _contact_no { get; set; }
+        [DisplayName("Contact Email")]
+        public string _contact_email { get; set; }
+        [DisplayName("Address")]
+        public string _address { get; set; }
+        [DisplayName("City")]
+        public string _city { get; set; }
+        [DisplayName("Car Model")]
+        public string _car_model { get; set; }
+        [DisplayName("Car Make")]
+        public string _car_make { get; set; }
+        [DisplayName("Car Year")]
+        public string _car_year { get; set; }
+    }
+}
